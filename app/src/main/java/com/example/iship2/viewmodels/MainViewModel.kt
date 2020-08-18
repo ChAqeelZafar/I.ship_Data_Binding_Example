a4e7e9fe12f4beb7356e059e620529e8859747d0
ChAqeelZafar <f178076@nu.edu.pk>
2020-08-18 14:56:27 +0500

    databinding 1-way & 2-way example code

--- app/src/main/java/com/example/iship2/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/iship2/viewmodels/MainViewModel.kt
@@ -8,15 +8,13 @@ import com.example.iship2.repositories.FakeRepository
 
 
 class MainViewModel : ViewModel() {
+
     val currentRandomFruitName : LiveData<String>
     get() = FakeRepository.currentRandomFruitName
-
-
     fun onChangeRandomFruitClick() = FakeRepository.changeCurrentRandomFruitName()
 
 
     val editTextContent = MutableLiveData<String>()
-
 
     private val _displayedEditTextContent = MutableLiveData<String>()
     val displyedEditTextContent : LiveData<String>
